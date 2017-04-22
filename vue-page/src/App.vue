80d549681bec23d680738169d4741b3e1428f9e6
ssthouse <ssthouse@163.com>
2017-04-22 23:06:41 +0800

    Try use vue project inside

---
+++ vue-page/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" style="top: 0; bottom: 0;">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
