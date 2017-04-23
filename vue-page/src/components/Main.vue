8d1585d750caac6e3891c9b24f3ca173e5c7870a
ssthouse <ssthouse@163.com>
2017-04-23 13:35:01 +0800

    Add simple tab bottom btns

---
+++ vue-page/src/components/Main.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-ons-page>
+      <v-ons-tabbar :visible="true"
+                    :index="activeIndex"
+                    @update="activeIndex = $event">
+
+        <v-ons-tab v-for="(tab, i) in tabs"
+                   :label="tabs[i].label"></v-ons-tab>
+
+        <template slot="pages">
+          <home></home>
+          <home></home>
+          <home></home>
+        </template>
+      </v-ons-tabbar>
+    </v-ons-page>
+  </div>
+</template>
+
+<script>
+  import Home from './home/Home.vue'
+  import Vue from 'vue'
+
+  Vue.component(Home.name, Home)
+
+  export default{
+    name: 'main',
+    data () {
+      return {
+        tabs: [{
+          label: 'Home'
+        }, {
+          label: 'news'
+        }, {
+          label: 'setting'
+        }],
+        activeIndex: 0
+      }
+    },
+    props: [],
+    methods: {},
+    computed: {},
+    created: function () {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vue-page/src/components/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-ons-page>
+    this is the home page
+  </v-ons-page>
+</template>
+
+<script>
+  export default{
+    name: 'home',
+    data () {
+      return {}
+    },
+    props: [],
+    methods: {},
+    computed: {},
+    created: function () {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
