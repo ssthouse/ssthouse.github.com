efbcae6c0058f122380b8ff9bf6988ad8589902d
ssthouse <ssthouse@163.com>
2017-12-18 22:17:41 +0800

    Fix fastnote UI

---
+++ vue-page/src/components/giant/EditNoteDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-dialog v-model="showDialog" persistent max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="onClickCloseButton()">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="onClickSaveButton()">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'edit-note-dialog',
+    data () {
+      return {}
+    },
+    props: ['showDialog'],
+    methods: {
+      onClickCloseButton () {
+
+      },
+      onClickSaveButton () {
+
+      }
+    },
+    computed: {},
+    created: function () {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- vue-page/src/components/giant/Giant.vue
+++ vue-page/src/components/giant/Giant.vue
@@ -1,36 +1,61 @@
 <template>
   <div style="width: 100%; height: 100%; display: block; position: relative">
     <div style="width: 100%; height: 100%; display: block; position: absolute;">
-      <mu-card style="height: 50%; width: 100%;">
+      <mu-card style="height: 50%; width: 100%; position: relative;">
         <div ref="littleNote"
              style="height: 100%; text-align: left; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;
            font-size: larger; overflow-y: scroll;">
           {{littleBabyNote}}
         </div>
+        <div style="position: absolute; right: 20px; bottom: 20px;">
+          <v-btn
+            @click.native="onOpenLittleDialog()"
+            fab
+            dark
+            color="pink">
+            <v-icon ref="fabIcon">edit</v-icon>
+          </v-btn>
+        </div>
       </mu-card>
+
       <v-divider></v-divider>
-      <mu-card style="height: 50%; width: 100%;">
+
+      <mu-card style="height: 50%; width: 100%; position: relative;">
         <div ref="giantNote"
              style="height: 100%; text-align: left; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;
            font-size: larger; overflow-y: scroll;">{{giantBabyNote}}
+        </div>
+        <div style="position: absolute; right: 20px; bottom: 20px;">
+          <v-btn
+            @click="onOpenGiantDialog()"
+            fab
+            dark
+            color="pink">
+            <v-icon ref="fabIcon">edit</v-icon>
+          </v-btn>
         </div>
       </mu-card>
 
       <mu-snackbar v-if="snackbar" message="获取fastnote出错 :|"></mu-snackbar>
     </div>
+
+    <edit-note-dialog :showDialog="showDialog"></edit-note-dialog>
   </div>
 </template>
 
 <script>
   import * as fastnote from '../cloud/fastnote'
+  import EditNoteDialog from './EditNoteDialog'
 
   export default{
+    components: {EditNoteDialog},
     name: 'favorite',
     data () {
       return {
         littleBabyNote: '稍等哦, 大宝在拼命加载...',
         giantBabyNote: '稍等哦, 大宝在拼命加载...',
-        snackbar: false
+        snackbar: false,
+        showDialog: false
       }
     },
     props: [],
@@ -39,6 +64,11 @@
         this.snackbar = true
         if (this.snackbar) clearTimeout(this.snackTimber)
         this.snackTimber = setTimeout(() => { this.snackbar = false }, 2000)
+      },
+      onOpenLittleDialog () {
+      },
+      onOpenGiantDialog () {
+
       }
     },
     computed: {},
@@ -61,5 +91,9 @@
 </script>
 
 <style>
-
+  .custom-fab {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+  }
 </style>
