1e8b962e6f7661657f5480e7fa1b5a130dfb9640
ssthouse <ssthouse@163.com>
2018-11-01 10:07:22 +0800

    Fix static file path

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../index.html'),
-    assetsRoot: path.resolve(__dirname, '../src'),
-    assetsSubDirectory: 'assets',
+    index: path.resolve(__dirname, '../index.html'),
+    assetsRoot: path.resolve(__dirname, '../'),
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
