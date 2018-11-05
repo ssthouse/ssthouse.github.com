cbfb3d72569cfb4171bb5105b49538e7c8658b64
ssthouse <ssthouse@163.com>
2018-11-05 22:32:47 +0800

    Add basic timeline component

---
+++ src/components/me/timeline/TimePiece.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="time-piece">
+    <div class="title-row">
+      <v-chip disabled="true" class="time-title">
+        <v-icon>event</v-icon>
+        {{timeTitle}}
+      </v-chip>
+      <span class="title">{{title}}</span>
+    </div>
+    <div class="content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['timeTitle', 'title'],
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.time-piece {
+  font-size: 1.6rem;
+
+  .title-row {
+    text-align: left;
+
+    .time-title {
+      i {
+        margin-right: 8px;
+      }
+      width: 120px;
+      font-size: 1.4rem;
+    }
+
+    .title {
+    }
+  }
+
+  .content {
+  }
+}
+</style>
+
--- src/components/me/timeline/Timeline.vue
+++ src/components/me/timeline/Timeline.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    hi, this is the basic timeline
+    <time-piece time-title="2013年" title="就读于华中科技大学自动化专业">
+      <div slot="content">content</div>
+    </time-piece>
+
+    <time-piece time-title="2015年" title="百无聊赖的国庆节, 自学起了Android开发">
+      <div slot="content">content</div>
+    </time-piece>
+
+    <time-piece time-title="2016年" title="通过通宵测试, 加入华中科技大学 Dian团队.">
+      <div slot="content">content</div>
+    </time-piece>
   </div>
 </template>
 <script>
+import TimePiece from './TimePiece.vue'
+
 export default {
+  components: { TimePiece },
   data() {
-    return {
-      timelineList: [
-        {
-          title: '2013年就读于华中科技大学'
-        }
-      ]
-    }
+    return {}
   }
 }
 </script>
