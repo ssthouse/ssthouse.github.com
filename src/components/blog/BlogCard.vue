b41689f5691bc2f079951115df42f47e2eec168d
ssthouse <ssthouse@163.com>
2018-11-07 23:21:21 +0800

    Add basic blog card

---
+++ src/components/blog/BlogCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="blog-card">
+    <span>{{blog.title}}</span>
+    <img class="blog-img" :src="blog.image" />
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['blog'],
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+.blog-card {
+  .blog-img{
+    
+  }
+}
+</style>
+
