ecb640c4e4284bd2babca8f8c11ea6ea889bb924
ssthouse <ssthouse@163.com>
2018-11-09 10:41:04 +0800

    Add badic project card style

--- src/components/code/Code.vue
+++ src/components/code/Code.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="">
-    <project-card></project-card>
+    <project-card v-for="project in projectList" :key="project.title" :project="project"></project-card>
   </div>
 
 </template>
@@ -11,7 +11,16 @@ import ProjectCard from './ProjectCard.vue'
 export default {
   components: { ProjectCard },
   data() {
-    return {}
+    return {
+      projectList: [
+        {
+          title: 'Github Repository 可视化',
+          description: '可视化展示个人及following people的github 仓库',
+          href: 'https://ssthouse.github.io/github-visualization/#/main',
+          img: require('../../assets/d3-blog-img/github-viz.png')
+        }
+      ]
+    }
   },
   props: [],
   methods: {},
--- src/components/code/ProjectCard.vue
+++ src/components/code/ProjectCard.vue
@@ -1,25 +1,57 @@
 <template>
   <v-card class="project-card">
-    <div v-bind:style="{'background-image':'url(' + imgUrl + ')' }">
-      <span>
-        description
-      </span>
+    <div class="img-container">
+      <img :src="project.img" />
     </div>
+    <a :href="project.href" class="project-title">
+      <span>
+        {{project.title}}
+      </span>
+    </a>
+
+    <span>
+      {{project.description}}
+    </span>
   </v-card>
 </template>
 <script>
-const imgUrl = require('../../assets/small_duo.jpg')
-
 export default {
+  props: ['project'],
   data() {
-    return {
-      imgUrl
-    }
+    return {}
   }
 }
 </script>
-<style lang="less">
+<style lang="less" scoped>
 .project-card {
   padding: 0;
+  margin: 16px;
+  position: relative;
+  width: 360px;
+  height: 480px !important;
+  display: flex;
+  flex-direction: column;
+
+  .img-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .project-title {
+    padding: 8px;
+    width: 100%;
+    text-align: left;
+    justify-self: flex-start;
+    font-size: 1.6rem;
+    z-index: 1;
+    background-color: rgba(238, 238, 238, 0.746);
+  }
 }
 </style>
