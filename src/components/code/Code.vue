fa1cadd432782594a60fd75999954484da43eac0
ssthouse <ssthouse@163.com>
2018-11-09 11:02:07 +0800

    Add more card to code panel

--- src/components/code/Code.vue
+++ src/components/code/Code.vue
@@ -18,6 +18,23 @@ export default {
           description: '可视化展示个人及following people的github 仓库',
           href: 'https://ssthouse.github.io/github-visualization/#/main',
           img: require('../../assets/d3-blog-img/github-viz.png')
+        },
+        {
+          title: '可视化讲解 DOM 构建过程',
+          href: 'https://ssthouse.github.io/visual-explain/#/list/domRender',
+          img:
+            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/dom-render/img/step9.png'
+        },
+        {
+          title: '可视化讲解 深度优先遍历 (DFT)',
+          href: 'https://ssthouse.github.io/visual-explain/#/list/dft',
+          img: require('../../assets/d3-blog-img/dft-demo.png')
+        },
+        {
+          title: '用 D3.js 画一个手机专利关系图',
+          href: 'https://ssthouse.github.io/visual-explain/#/list/patent-suit',
+          img:
+            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/mobile-patent-suit/img/row_display.png'
         }
       ]
     }
--- src/components/code/ProjectCard.vue
+++ src/components/code/ProjectCard.vue
@@ -3,13 +3,13 @@
     <div class="img-container">
       <img :src="project.img" />
     </div>
-    <a :href="project.href" class="project-title">
+    <a :href="project.href" class="project-title" target="_blank">
       <span>
         {{project.title}}
       </span>
     </a>
 
-    <span>
+    <span class="project-description" v-if="project.description">
       {{project.description}}
     </span>
   </v-card>
@@ -29,8 +29,9 @@ export default {
   position: relative;
   width: 360px;
   height: 480px !important;
-  display: flex;
+  display: inline-flex;
   flex-direction: column;
+  justify-content: space-between;
 
   .img-container {
     display: flex;
@@ -51,6 +52,26 @@ export default {
     justify-self: flex-start;
     font-size: 1.6rem;
     z-index: 1;
+    background-color: rgba(238, 238, 238, 0.746);
+  }
+
+  .project-description {
+    justify-self: flex-end;
+    padding: 4px;
+    width: 100%;
+    text-align: left;
+    z-index: 1;
+    transition-property: opacity, background-color;
+    transition-duration: 1s;
+    background-color: transparent;
+    font-size: 1.4rem;
+    opacity: 0;
+  }
+}
+
+.project-card:hover {
+  .project-description {
+    opacity: 1;
     background-color: rgba(238, 238, 238, 0.746);
   }
 }
