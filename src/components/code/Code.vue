18f42f6cf0095b20c5857de2092af6e98291df65
ssthouse <ssthouse@163.com>
2018-11-10 17:39:56 +0800

    Finish code panel

--- src/components/code/Code.vue
+++ src/components/code/Code.vue
@@ -17,24 +17,32 @@ export default {
           title: 'Github Repository 可视化',
           description: '可视化展示个人及following people的github 仓库',
           href: 'https://ssthouse.github.io/github-visualization/#/main',
-          img: require('../../assets/d3-blog-img/github-viz.png')
+          sourceCodeHref: 'https://github.com/ssthouse/github-visualization',
+          img:
+            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/github-visualization/img/visual-github-repo.gif'
         },
         {
           title: '可视化讲解 DOM 构建过程',
+          description: '可视化分析浏览器构建DOM的过程',
           href: 'https://ssthouse.github.io/visual-explain/#/list/domRender',
+          sourceCodeHref: 'https://github.com/ssthouse/visual-explain',
           img:
             'https://raw.githubusercontent.com/ssthouse/d3-blog/master/dom-render/img/step9.png'
         },
         {
-          title: '可视化讲解 深度优先遍历 (DFT)',
+          title: '可视化讲解 深度优先遍历',
+          description: '可视化分析深度遍历过程中栈的变化',
           href: 'https://ssthouse.github.io/visual-explain/#/list/dft',
-          img: require('../../assets/d3-blog-img/dft-demo.png')
+          sourceCodeHref: 'https://github.com/ssthouse/visual-explain',
+          img:
+            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/viz-depth-first-traversal/img/demo.gif'
         },
         {
           title: '用 D3.js 画一个手机专利关系图',
           href: 'https://ssthouse.github.io/visual-explain/#/list/patent-suit',
+          sourceCodeHref: 'https://github.com/ssthouse/visual-explain',
           img:
-            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/mobile-patent-suit/img/row_display.png'
+            'https://raw.githubusercontent.com/ssthouse/d3-blog/master/mobile-patent-suit/img/demo.gif'
         }
       ]
     }
--- src/components/code/ProjectCard.vue
+++ src/components/code/ProjectCard.vue
@@ -1,13 +1,18 @@
 <template>
   <v-card class="project-card">
     <div class="img-container">
-      <img :src="project.img" />
+      <img :src="project.img" height="100%" />
     </div>
-    <a :href="project.href" class="project-title" target="_blank">
-      <span>
-        {{project.title}}
-      </span>
-    </a>
+    <div class="project-title">
+      <a :href="project.href" target="_blank">
+        <span>
+          {{project.title}}
+        </span>
+      </a>
+      <a target="_blank" class="github-link" :href="project.sourceCodeHref">
+        <img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" />
+      </a>
+    </div>
 
     <span class="project-description" v-if="project.description">
       {{project.description}}
@@ -46,6 +51,9 @@ export default {
   }
 
   .project-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     padding: 8px;
     width: 100%;
     text-align: left;
@@ -53,6 +61,25 @@ export default {
     font-size: 1.6rem;
     z-index: 1;
     background-color: rgba(238, 238, 238, 0.746);
+
+    .github-link {
+      margin-left: 8px;
+
+      img {
+        transition-property: box-shadow;
+        transition-duration: 1s;
+        border-radius: 50%;
+        width: 32px;
+        height: 32px;
+      }
+    }
+
+    .github-link:hover {
+      img {
+        box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.2),
+          0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 2px 3px 0 rgba(0, 0, 0, 0.12);
+      }
+    }
   }
 
   .project-description {
