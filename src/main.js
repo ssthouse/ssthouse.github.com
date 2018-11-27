8aaa7f78f4d22eb26ab0850d9efce414f083c406
ssthouse <ssthouse@163.com>
2018-11-27 11:46:17 +0800

    Using vuetify's min-css

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Storage from './store/storage'
 import Eventbus from './components/utils/EventBus'
 import store from './store/index'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import 'vuetify/dist/vuetify.min.css'
 import i18n from './i18n/index'
 
 Vue.config.productionTip = false
